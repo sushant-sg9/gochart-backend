a52c2b19edfbfa5e2b7141649744482ebefe8585
Sushant <sushantghadge2016@gmail.com>
2026-01-02 17:08:07 +0530

    change app.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
 import app from './app.js';
 import { connectDB } from './config/database.js';
 import cronJobs from './services/cronJobs.js';
